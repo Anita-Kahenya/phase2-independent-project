059ef761ad0e49496b320fb5fddef2e86f69e843
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-01-25 15:37:29 +0300

    search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SelectYear from './SelectYear';
 import Finalists from "./Finalists";
 import WinnerCard from "./WinnerCard";
 
 function Home(){
+    const [tournaments, setTournaments] = useState([])
+    useEffect(()=> {
+    fetch('http://localhost:8001/data')
+    .then((resp) => resp.json())
+    .then((data) => setTournaments(data))
+    }, [])
     return(
         <div>
-            <SelectYear/>
+            <SelectYear tournaments = {tournaments}/>
             <Finalists/>
             <WinnerCard/>
         </div>
--- src/components/SelectYear.js
+++ src/components/SelectYear.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-function SelectYear(){
+function SelectYear({ tournaments }){
     return(
-        <div className="input-group mb-3">
+        <div className="input-group mb-3" id='searchBar'>
             <div className="input-group-prepend">
-                <label className="input-group-text" for="inputGroupSelect01">Options</label>
+                <label className="input-group-text" for="inputGroupSelect01">Select Year</label>
             </div>
-            <select className="custom-select" id="inputGroupSelect01">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+            <select  className="custom-select search-field" id="inputGroupSelect01">
+            {
+                tournaments.map((tournament, index)=>{
+                    return(
+                        <option key={index} value={tournament.year}>{tournament.year}</option>
+                    )
+                })
+            }
             </select>
         </div>    
     )
